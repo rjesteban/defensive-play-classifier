2e4a7c6f17c6be49cbde1612b800efdf601b6966
Arnold Joseph Esteban <ajcp.esteban@gmail.com>
2017-03-09 16:28:07 +0800

    Update matchups.py

--- eda/matchups.py
+++ eda/matchups.py
@@ -7,12 +7,21 @@ def count_matchup_over_time(gid="0021500582", eid=30):
     frames = len(action.coords)
 
     color_dict = {
-        0: 'y',  # center
-        1: 'g',  # forward
-        2: 'c',  # forward
-        3: 'b',  # guard
         4: 'r',  # guard
+        3: 'b',  # guard
+        2: 'g',  # forward
+        1: 'm',  # forward
+        0: 'y'  # center
     }
+
+    # defenders (y axis)
+    # 4 guard
+    # 3 guard
+    # 2 forward
+    # 1 forward
+    # 0 center
+
+    # time (x axis)
 
     import matplotlib.patches as mpatches
     import matplotlib.pyplot as plt
@@ -21,10 +30,10 @@ def count_matchup_over_time(gid="0021500582", eid=30):
         for deff in range(5):
             for off in range(5):
                 if matchups[deff][off] == 1:
-                    plt.plot([frame], [deff + 1], 's', color=color_dict[off])
+                    plt.plot([frame], [deff], 's', color=color_dict[off])
                     # defended[off].append(off)
                     # color[off].append(color_dict[deff])
-    plt.ylim(0, 6)
+    plt.ylim(-0.5, 5.5)
     center = mpatches.Patch(color=color_dict[0], label='Center')
     powerf = mpatches.Patch(color=color_dict[1], label='Power Forward')
     smallf = mpatches.Patch(color=color_dict[2], label='Small Forward')
