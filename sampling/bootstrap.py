29ab7910f09e117e7dbd4c48ecff286a225c0ea5
rjesteban <ajcp.esteban@gmail.com>
2017-04-05 21:59:35 +0800

    Edit type returned

--- sampling/bootstrap.py
+++ sampling/bootstrap.py
@@ -1,12 +1,15 @@
+import numpy as np
+import random
+
+
 def bootstrap632(X, y):
-    import random
     d = len(y)
     training_indices = []
     for index in range(d):
         training_indices.append(random.randint(0, d - 1))
     testing_indices = [i for i in range(d) if i not in training_indices]
-    train_X = [X[i] for i in training_indices]
-    train_y = [y[i] for i in training_indices]
-    test_X = [X[i] for i in testing_indices]
-    test_y = [y[i] for i in testing_indices]
+    train_X = np.concatenate([X[i] for i in training_indices])
+    train_y = np.array([y[i] for i in training_indices])
+    test_X = np.concatenate([X[i] for i in testing_indices])
+    test_y = np.array([y[i] for i in testing_indices])
     return train_X, test_X, train_y, test_y
