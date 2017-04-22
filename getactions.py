5cd2c094865c8b226f18d2058a071675b1180739
rjesteban <ajcp.esteban@gmail.com>
2017-04-23 01:12:22 +0800

    Set check frames to true

--- getactions.py
+++ getactions.py
@@ -69,7 +69,7 @@ for gid in sorted(zonedefs.keys()):
     for eid in zonedefs[gid]:
         try:
             action = convert_moment_to_action(data, eid,
-                                              check_frames=False, label=-1)
+                                              check_frames=True, label=-1)
             action.save()
             run(eid=eid, gid=gid, act=action)
         except Exception:
@@ -83,7 +83,7 @@ for gid in sorted(mandefs.keys()):
     for eid in mandefs[gid]:
         try:
             action = convert_moment_to_action(data, eid,
-                                              check_frames=False, label=1)
+                                              check_frames=True, label=1)
             action.save()
             run(eid=eid, gid=gid, act=action)
         except Exception:
